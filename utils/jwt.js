857c063a609067a3c3e75984c845059530b2b30f
Safayet <hossainsafayet187@gmail.com>
2024-11-23 01:36:39 +0600

    jwt fixed

--- utils/jwt.js
+++ utils/jwt.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const createJWT = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
     res.cookie("token", token, {
-        httpOnly: false,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "None",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Ensures it is sent only over HTTPS in production
+        sameSite: 'None',
         maxAge: 60 * 60 * 24 * 1000 // 1 day in milliseconds
     });
 }
