6f7e4c66991bdff5d3f9a2e6e923928209a6d8a1
Safayet <hossainsafayet187@gmail.com>
2024-11-23 01:50:43 +0600

    jwt fixed again

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,11 +4,10 @@ const createJWT = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Ensures it is sent only over HTTPS in production
+        secure: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development', // Use secure in both production and development for testing
         sameSite: 'None',
         maxAge: 60 * 60 * 24 * 1000 // 1 day in milliseconds
     });
 }
-
 
 module.exports = createJWT;
