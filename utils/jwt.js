f1b0ad62139c620d71218e2713ac2d5da54c99ac
Safayet <hossainsafayet187@gmail.com>
2024-11-23 23:39:57 +0600

    jwt fixed

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,25 +1,24 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const createJWT = (res, userId) => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined in the environment variables.");
-  }
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in the environment variables.");
+    }
 
-  try {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Secure only in production
-      sameSite: "None",
-      domain: ".onrender.com",
-      maxAge: 60 * 60 * 24 * 1000, // 1 day
-    });
-  } catch (error) {
-    console.error("Error creating JWT:", error);
-    throw new Error("Failed to create JWT");
-  }
+    try {
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // Secure only in production
+            sameSite: 'None',
+            maxAge: 60 * 60 * 24 * 1000, // 1 day
+        });
+   
+
+    } catch (error) {
+        console.error("Error creating JWT:", error);
+        throw new Error("Failed to create JWT");
+    }
 };
 
 module.exports = createJWT;
