97ee4a1471ec80ba2c576d86a9737d29aee539a5
Safayet <hossainsafayet187@gmail.com>
2024-11-24 16:06:11 +0600

    change in jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -6,12 +6,12 @@ const createJWT = (res, userId) => {
     }
 
     try {
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Secure only in production
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'None',
-            maxAge: 60 * 60 * 24 * 1000, // 1 day
+            maxAge: 60 * 60 * 24 * 30 * 1000
         });
    
 
