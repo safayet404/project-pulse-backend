78770aff74b0408cf8beca22dcf05cd02e24c520
Safayet <hossainsafayet187@gmail.com>
2024-11-24 20:16:30 +0600

    maxAge alignment fixed

--- utils/jwt.js
+++ utils/jwt.js
@@ -11,7 +11,7 @@ const createJWT = (res, userId) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'None',
-            maxAge: 60 * 60 * 24 * 30 * 1000
+            maxAge: 30 * 24 * 60 * 60 * 1000
         });
    
 
