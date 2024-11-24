33cc500d881db2aa359474a2f6c47cefeb766263
Safayet <hossainsafayet187@gmail.com>
2024-11-24 20:20:19 +0600

    token duration issue

--- utils/jwt.js
+++ utils/jwt.js
@@ -6,12 +6,12 @@ const createJWT = (res, userId) => {
     }
 
     try {
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'None',
-            maxAge: 30 * 24 * 60 * 60 * 1000
+            maxAge: 60 * 60 * 24 * 1000
         });
    
 
